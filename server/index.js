c3eaa7502130f68c428163d8f6bfce9b515c8bdc
Sranjan0208 <sranjan0208@gmail.com>
2024-04-10 17:48:29 +0530

    Production Ready

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["*"],
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
